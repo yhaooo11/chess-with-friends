8d944bbe19eb8d545797071820b56b8320842e63
patrickhong21 <patrickhong21@hotmail.com>
2022-09-03 16:36:05 -0700

    added Tile toChessCoords

--- cwf-frontend/src/models/Tile.js
+++ cwf-frontend/src/models/Tile.js
@@ -1,11 +1,22 @@
 export default class Tile {
     constructor(row, column, piece=null) {
-        this.piece = piece;
         this.row = row;
         this.column = column;
+        this.piece = piece;
     }
     // converts (x,y) to algebraic notation (eg. a1) 
     toChessCoords() {
+        const letterMap = {
+            0: 'a',
+            1: 'b',
+            2: 'c',
+            3: 'd',
+            4: 'e',
+            5: 'f',
+            6: 'g',
+            7: 'h'
+        }
 
+        return "" + letterMap[this.column] + (8 - this.row);
     }
 }
