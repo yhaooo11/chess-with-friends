0287f39f8bcd1e0336bb273392b1a22392044dfc
patrickhong21 <patrickhong21@hotmail.com>
2022-09-03 17:59:32 -0700

    king valid moves

--- cwf-frontend/src/models/pieces/King.js
+++ cwf-frontend/src/models/pieces/King.js
@@ -5,7 +5,20 @@ export default class King extends Piece {
         super(color);
         this.castled = false;
     }
+
     getInitials() {
         return this.color + 'K';
+    }
+
+    getValidMoves(board, currentTile) {
+        const moves = [[-1, -1], [-1, 0], [-1, 1], [0, 1], [1, 1], [0, -1], [-1, -1], [-1, 0]];
+        const validMoves = moves.map(move => 
+            [currentTile.row + move[0], currentTile.column + move[1]]).filter(move => 
+                this.isInBounds(move)).filter(move => {
+                    const algebraicMove = this.toChessCoords(move);
+                    return board.tiles[algebraicMove].piece === null || board.tiles[algebraicMove].piece.color !== this.color;
+        });
+
+        return validMoves;
     }
 }
